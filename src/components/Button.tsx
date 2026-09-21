d75ec917e06f8db4ec9130bb753afe7ca4d588d4
z4gon <4588601+z4gon@users.noreply.github.com>
2026-09-21 03:24:56 -0300

    button

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import type { HTMLAttributes } from "preact";
+import styles from "./Button.module.scss";
+import { classNames } from "../utils/classNames";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+const Button = ({ className, onClick, children, ...rest }: ButtonProps) => {
+  return (
+    <button
+      className={classNames(styles.button, className)}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
