ee6fbc4797ddbedbb9d30224ee8044df11d4d576
z4gon <4588601+z4gon@users.noreply.github.com>
2026-09-22 03:11:46 -0300

    enums

---
+++ src/models/enums/Category.ts
@@ -0,0 +1,8 @@
+enum Category {
+  Shaders = 'Shaders',
+  ComputeShaders = 'Compute Shaders',
+  VisualEffects = 'Visual Effects',
+  GameDev = 'Game Dev',
+}
+
+export default Category
---
+++ src/models/enums/Technology.ts
@@ -0,0 +1,8 @@
+enum Technology {
+  UnityEngine = 'UnityEngine',
+  UnrealEngine = 'UnrealEngine',
+  Metal = 'Metal',
+  GodotEngine = 'GodotEngine',
+}
+
+export default Technology
